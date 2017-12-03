48e1d61e76d08810a5038caf5271fab42c69f05f
mart00n <awmarton@gmail.com>
2017-12-02 18:14:00 -0600

    Hello y2k!

---
+++ htooze/world.py
@@ -0,0 +1,5 @@
+class Planet():
+    def __init__(self):
+        return
+
+
--- tests/test_world.py
+++ tests/test_world.py
@@ -3,3 +3,4 @@ from htooze import world
 def test_planet_exists():
     p = world.Planet()
     assert isinstance(p, world.Planet)
+
