64a8b45f66a4dd85a39c7e9c97c9db467f74c24b
y2k <git@y2kbugger.com>
2017-12-10 19:20:38 -0500

    tests: Add tests for cells
    Tests for cells and how they interact with the planet.
    
    You will have slightly more freedom on how to implement, but it is still
    fairly straight forward. Once we get more into the groove of this things
    will get more and more open end.
    
    Also we will do code review directly on the master branch right now.
    There are more proper workflows that involve multiple branches and
    rewriting commits but I will keep it simple for now. The only reason
    (for now) that I will ask you rewrite commits is if the message really
    sucks. If that happens I will call you and walk you through it.

--- tests/test_world.py
+++ tests/test_world.py
@@ -4,3 +4,21 @@ def test_planet_exists():
     p = world.Planet()
     assert isinstance(p, world.Planet)
 
+def test_life_can_exist():
+    mycell = world.Cell()
+    assert isinstance(mycell, world.Cell)
+
+def test_planet_starts_without_life():
+    p = world.Planet()
+    assert len(p.life) == 0
+
+def test_life_can_live_on_planet():
+    p = world.Planet()
+    mycell = world.Cell()
+    p.addcell(mycell)
+
+    assert len(p.life) == 1
+    for coords, cell in p.life.items():
+        assert cell is mycell
+        assert isinstance(coords, tuple)
+        assert int(coords[0]) == coords[0]
