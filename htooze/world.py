0e569ea2bc4ac00f7042eba2692e8c8685fe4781
mart00n <awmarton@gmail.com>
2018-04-05 19:57:24 -0500

    Added attributes to world classes

--- htooze/world.py
+++ htooze/world.py
@@ -1,5 +1,23 @@
+import random
+
 class Planet():
+    def __init__(self, size=(100, 100)):    
+        self.life = {}
+        self.size = (100, 100)
+    def addcell(self, cell):
+        ''' Add a cell to the planet at random position
+        
+        Parameters
+        ----------
+        cell : htooze.world.Cell
+            A cell is added to the world
+
+
+        '''
+        position = tuple(random.randint(0, n) for n in self.size)
+        self.life[position] = cell
+        print(position)
+
+class Cell():
     def __init__(self):
-        return
-
-
+        pass
