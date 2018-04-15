a65a852b2bd5bcc2bb5b099bc20020e8697ca2cc
y2k <git@y2kbugger.com>
2018-04-15 02:43:32 -0400

    Strip whitespace in htooze.world

--- htooze/world.py
+++ htooze/world.py
@@ -1,12 +1,12 @@
 import random
 
 class Planet():
-    def __init__(self, size=(100, 100)):    
+    def __init__(self, size=(100, 100)):
         self.life = {}
         self.size = (100, 100)
     def addcell(self, cell):
         ''' Add a cell to the planet at random position
-        
+
         Parameters
         ----------
         cell : htooze.world.Cell
